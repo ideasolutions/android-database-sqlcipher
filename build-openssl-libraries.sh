7ac49cfe6164707f916368d13d456b0bb5680350
Luca Looz <luca.looz92@gmail.com>
2017-09-09 09:15:14 +0200

    fix openssl build script: - minimum api level 15 - exclusion options - compilation flags - removed 64 bit versions (will fix later when needed with correct compilation flags)

--- build-openssl-libraries.sh
+++ build-openssl-libraries.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+git submodule update --init
 (cd external/openssl;
 
     if [ ! ${ANDROID_NDK_ROOT} ]; then
@@ -7,14 +8,65 @@
     fi
 
     ANDROID_LIB_ROOT=../android-libs
-    ANDROID_TOOLCHAIN_DIR=/tmp/sqlcipher-android-toolchain
-    OPENSSL_CONFIGURE_OPTIONS="no-krb5 no-idea no-camellia \
-        no-seed no-bf no-cast no-rc2 no-rc4 no-rc5 no-md2 \
-        no-md4 no-ripemd no-rsa no-ecdh no-sock no-ssl2 no-ssl3 \
-        no-dsa no-dh no-ec no-ecdsa no-tls1 no-pbe no-pkcs \
-        no-tlsext no-pem no-rfc3779 no-whirlpool no-ui no-srp \
-        no-ssltrace no-tlsext no-mdc2 no-ecdh no-engine \
-        no-tls2 no-srtp -fPIC"
+    ANDROID_TOOLCHAIN_DIR=/tmp/openssl-android-toolchain
+    OPENSSL_OPTIONS="\
+        no-afalgeng \
+        no-bf \
+        no-blake2 \
+        no-camellia \
+        no-capieng \
+        no-cast \
+        no-chacha \
+        no-cmac \
+        no-cms \
+        no-comp \
+        no-ct \
+        no-des \
+        no-dgram \
+        no-dh \
+        no-dsa \
+        no-dso \
+        no-dtls \
+        no-dynamic-engine \
+        no-ec \
+        no-ec2m \
+        no-ecdh \
+        no-ecdsa \
+        no-engine \
+        no-gost \
+        no-hw \
+        no-idea \
+        no-md4 \
+        no-mdc2 \
+        no-nextprotoneg \
+        no-ocb \
+        no-ocsp \
+        no-poly1305 \
+        no-rc2 \
+        no-rc4 \
+        no-rfc3779 \
+        no-rmd160 \
+        no-shared \
+        no-sock \
+        no-srp \
+        no-srtp \
+        no-sse2 \
+        no-ssl \
+        no-static-engine \
+        no-tls \
+        no-ts \
+        no-ui \
+        no-whirlpool"
+
+    DEFAULT_FLAGS=" \
+        -ffunction-sections \
+        -funwind-tables \
+        -fstack-protector-strong \
+        -g \
+        -O2 \
+        -no-canonical-prefixes"
+
+    DEFAULT_ARM_FLAGS="-mthumb -fpic"
     
     HOST_INFO=`uname -a`
     case ${HOST_INFO} in
@@ -36,81 +88,73 @@
     esac
 
     rm -rf ${ANDROID_LIB_ROOT}
-    git clean -dfx && git checkout -f
-    ./Configure dist
-    
-    for SQLCIPHER_TARGET_PLATFORM in armeabi armeabi-v7a x86 x86_64 arm64-v8a
+    #git clean -dfx && git checkout -f
+
+    for TARGET_PLATFORM in armeabi armeabi-v7a x86
     do
-        echo "Building for libcrypto.a for ${SQLCIPHER_TARGET_PLATFORM}"
-        case "${SQLCIPHER_TARGET_PLATFORM}" in
+        echo "Building for libcrypto.a for ${TARGET_PLATFORM}"
+        case "${TARGET_PLATFORM}" in
             armeabi)
                 TOOLCHAIN_ARCH=arm
                 TOOLCHAIN_PREFIX=arm-linux-androideabi
-                CONFIGURE_ARCH=android
+                OPENSSL_TARGET="android-armeabi -D__ARM_MAX_ARCH__=8"
+                OPENSSL_FLAGS="$DEFAULT_FLAGS $DEFAULT_ARM_FLAGS -march=armv5te -mtune=xscale -msoft-float"
                 PLATFORM_OUTPUT_DIR=armeabi
-                ANDROID_PLATFORM_VERSION=android-9
+                ANDROID_API=15
                 ;;
             armeabi-v7a)
                 TOOLCHAIN_ARCH=arm
                 TOOLCHAIN_PREFIX=arm-linux-androideabi
-                CONFIGURE_ARCH=android -march=armv7-a
+                OPENSSL_TARGET="android-armeabi -D__ARM_MAX_ARCH__=8"
+                OPENSSL_FLAGS="$DEFAULT_FLAGS $DEFAULT_ARM_FLAGS -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -Wl,--fix-cortex-a8"
                 PLATFORM_OUTPUT_DIR=armeabi-v7a
-                ANDROID_PLATFORM_VERSION=android-9
+                ANDROID_API=15
                 ;;
             x86)
                 TOOLCHAIN_ARCH=x86
                 TOOLCHAIN_PREFIX=i686-linux-android
-                CONFIGURE_ARCH=android-x86
+                OPENSSL_TARGET=android-x86
+                OPENSSL_FLAGS=$DEFAULT_FLAGS
                 PLATFORM_OUTPUT_DIR=x86
-                ANDROID_PLATFORM_VERSION=android-9
-                ;;
-            x86_64)
-                TOOLCHAIN_ARCH=x86_64
-                TOOLCHAIN_PREFIX=x86_64-linux-android
-                CONFIGURE_ARCH=android64
-                PLATFORM_OUTPUT_DIR=x86_64
-                ANDROID_PLATFORM_VERSION=android-21
-                ;;
-            arm64-v8a)
-                TOOLCHAIN_ARCH=arm64
-                TOOLCHAIN_PREFIX=aarch64-linux-android
-                CONFIGURE_ARCH=android64-aarch64
-                PLATFORM_OUTPUT_DIR=arm64-v8a
-                ANDROID_PLATFORM_VERSION=android-21
-                ;;                
+                ANDROID_API=15
+                ;;        
             *)
-                echo "Unsupported build platform:${SQLCIPHER_TARGET_PLATFORM}"
+                echo "Unsupported build platform:${TARGET_PLATFORM}"
                 exit 1
         esac
 
         rm -rf ${ANDROID_TOOLCHAIN_DIR}
-        mkdir -p "${ANDROID_LIB_ROOT}/${SQLCIPHER_TARGET_PLATFORM}"
-        ${ANDROID_NDK_ROOT}/build/tools/make-standalone-toolchain.sh \
-                           --platform=${ANDROID_PLATFORM_VERSION} \
-                           --install-dir=${ANDROID_TOOLCHAIN_DIR} \
-                           --arch=${TOOLCHAIN_ARCH}
-
+        
+        ${ANDROID_NDK_ROOT}/build/tools/make_standalone_toolchain.py \
+            --arch ${TOOLCHAIN_ARCH} \
+            --api ${ANDROID_API} \
+            --install-dir "${ANDROID_TOOLCHAIN_DIR}"
+        
         export PATH=${ANDROID_TOOLCHAIN_DIR}/bin:$PATH
         export CROSS_SYSROOT=${ANDROID_TOOLCHAIN_DIR}/sysroot
-    
-        RANLIB=${TOOLCHAIN_PREFIX}-ranlib \
-              AR=${TOOLCHAIN_PREFIX}-ar \
-              CC=${TOOLCHAIN_PREFIX}-gcc \
-              ./Configure "${CONFIGURE_ARCH}" "${OPENSSL_CONFIGURE_OPTIONS}"
 
-        if [ $? -ne 0 ]; then
-            echo "Error executing:./Configure ${CONFIGURE_ARCH} ${OPENSSL_CONFIGURE_OPTIONS}"
-            exit 1
-        fi
-        
         make clean
-        make
+
+        RANLIB=${TOOLCHAIN_PREFIX}-ranlib \
+            AR=${TOOLCHAIN_PREFIX}-ar \
+            CC="${TOOLCHAIN_PREFIX}-gcc -D__ANDROID_API__=${ANDROID_API}" \
+            ./Configure $OPENSSL_TARGET $OPENSSL_OPTIONS ${DEFAULT_FLAGS} 
 
         if [ $? -ne 0 ]; then
-            echo "Error executing make for platform:${SQLCIPHER_TARGET_PLATFORM}"
+            echo "Error executing:./Configure ${OPENSSL_TARGET} ${OPENSSL_FLAGS} ${OPENSSL_OPTIONS}"
             exit 1
         fi
         
+        make build_libs
+
+        if [ $? -ne 0 ]; then
+            echo "Error executing make for platform:${TARGET_PLATFORM}"
+            exit 1
+        fi
+        
+        mkdir -p "${ANDROID_LIB_ROOT}/${PLATFORM_OUTPUT_DIR}/include"
         mv libcrypto.a ${ANDROID_LIB_ROOT}/${PLATFORM_OUTPUT_DIR}
-    done    
+        cp -r include/openssl "${ANDROID_LIB_ROOT}/${PLATFORM_OUTPUT_DIR}/include"
+    done
+
 )
